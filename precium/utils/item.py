c1e891db6603697293af46d3cc3751ecbd581386
Tobias Egelund <egelundtobias@gmail.com>
2022-11-11 22:26:47 +0100

    Refractor code

--- precium/utils/item.py
+++ precium/utils/item.py
@@ -1,6 +1,7 @@
 import json
 import attrs
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
+
 
 @attrs.define(frozen=True)
 class Item:
@@ -11,26 +12,39 @@ class Item:
     def serialize(self):
         return json.dumps(attrs.asdict(self))
 
+
 @attrs.define
 class ItemStatic(Item):
     uid: int = attrs.field(validator=attrs.validators.instance_of(int))
     category: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
-    product_main_group: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
-    product_sub_group: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
-    description: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
+    product_main_group: str = attrs.field(
+        default="", validator=attrs.validators.instance_of(str)
+    )
+    product_sub_group: str = attrs.field(
+        default="", validator=attrs.validators.instance_of(str)
+    )
+    description: str = attrs.field(
+        default="", validator=attrs.validators.instance_of(str)
+    )
     brand: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
     tags: List[str] = attrs.field(default=[])
-    unit_price_label: str = attrs.field(default="", validator=attrs.validators.instance_of(str))
+    unit_price_label: str = attrs.field(
+        default="", validator=attrs.validators.instance_of(str)
+    )
 
 
 @attrs.define
 class ItemPrice(Item):
     uid: int = attrs.field(validator=attrs.validators.instance_of(int))
     base_price: float = attrs.field(validator=attrs.validators.instance_of(float))
-    current_price: float = attrs.field(validator=attrs.validators.instance_of(float)) # if any campaign price
+    current_price: float = attrs.field(
+        validator=attrs.validators.instance_of(float)
+    )  # if any campaign price
     unit_price: float = attrs.field(validator=attrs.validators.instance_of(float))
-    current_unit_price: float = attrs.field(validator=attrs.validators.instance_of(float))
-    discount: float = attrs.field(validator=attrs.validators.instance_of(float))
+    current_unit_price: float = attrs.field(
+        validator=attrs.validators.instance_of(float)
+    )
+    discount: float = attrs.field(default=0.0)
 
     def _unpack_campaign(self, resp: Dict[str, Any]) -> Dict[str, Any]:
         raise NotImplementedError()
@@ -66,19 +80,29 @@ class NemligItemPrice(ItemPrice):
         "unit_price": "UnitPriceCalc",
         "current_price": "CampaignPrice",
         "current_unit_price": "CampaignUnitPrice",
-        "discount": "DiscountSavings"
+        "discount": "DiscountSavings",
     }
 
     @classmethod
     def new(cls, resp: Dict[str, Any]):
         build_dict = {}
 
+        campaign = cls._unpack_campaign(resp=resp)
+        if campaign is None:
+            # If no campaign - current price = base price
+            cls.kw_mapping.update(
+                {
+                    "current_price": "Price",
+                    "current_unit_price": "UnitPriceCalc",
+                }
+            )
+
         for key, kw in cls.kw_mapping:
             val = resp.get(kw, None)
             if val is None:
                 campaign = cls._unpack_campaign(resp=resp)
-                if campaign is None:
-                    pass
+                if campaign is not None:
+                    val = campaign.get(kw, None)
             build_dict.update({key: val})
 
         return cls(**resp)
